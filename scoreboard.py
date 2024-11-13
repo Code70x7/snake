7ae60be50f847dcfd1b4cb121d426528bc3ea231
Tony Dayton <tonylynndayton@yahoo.com>
2024-11-12 21:02:44 -0700

    Final Final Upload

--- scoreboard.py
+++ scoreboard.py
@@ -1,30 +1,3 @@
-# from turtle import Turtle
-# ALIGNMENT = "center"
-# FONT = ("Courier", 24, "normal")
-# #setting up the font for the score board
-# class Scoreboard(Turtle):
-#     def __init__(self):
-#         super().__init__()
-#         self.score = 0
-#         self.color("white")
-#         #erases the line up the middle
-#         self.penup()
-#         self.goto(0, 270)
-#         self.hideturtle()
-#         self.update_scoreboard()
-
-#     def update_scoreboard(self):
-#         #added variables for alignment and font
-#         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-#     def game_over(self):
-#         self.goto(0, 0)
-#         self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-#     def increase_score(self):
-#         self.score += 1
-#         self.clear()
-#         self.update_scoreboard()
-
-# scoreboard.py
 from turtle import Turtle
 
 class Scoreboard(Turtle):
